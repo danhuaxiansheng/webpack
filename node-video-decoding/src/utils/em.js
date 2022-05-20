c8c2630c98f64c711ea1088753da65da6fe7143f
danhuaxiansheng <646042371@qq.com>
2022-05-20 16:57:36 +0800

    d

--- node-video-decoding/src/utils/em.js
+++ node-video-decoding/src/utils/em.js
@@ -21,7 +21,7 @@
 
 // 解析b站视频   优酷
 // https://jx.parwix.com:4433/player/analysis.php?v=https://www.bilibili.com/bangumi/play/ep409856/
-
+// https://jx.parwix.com:4433/player/?url=https://www.bilibili.com/bangumi/play/ep409856/
 
 // 解析 爱奇艺视频  -优酷
 // https://api.okjx.cc:3389/jx.php?url=https://www.iqiyi.com/v_26cxipz70mw.html?vfrm=pcw_playpage&vfrmblk=D&vfrmrst=80521_listbox_positive
